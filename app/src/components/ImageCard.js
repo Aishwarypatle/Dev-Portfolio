d4746e50928242f602fb6e4ffc28f82b34790c11
Aishwary <aishwarypatle@26gmail.com>
2024-11-20 00:51:16 +0530

    fox: image updated

--- app/src/components/ImageCard.js
+++ app/src/components/ImageCard.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import Image from 'next/image';
-import userPic from '../assets/images/user_2.png'
+import userPic from '../assets/images/aishhhppp (1).png'
 
 const ImageCard = () => {
   return (
     <>
         <div class="h-80 w-80 relative">
-          <div class="absolute h-80 w-60 top-10 left-6 overflow-hidden z-10">
+          <div class="absolute h-80 w-60 top-4 left-4 overflow-hidden z-10">
             <Image
               src={userPic} 
               alt="Aishwary Patle" 
@@ -15,9 +15,9 @@ const ImageCard = () => {
               height="3434" decoding="async" data-nimg="1"  
               />
           </div>
-          <div class="absolute h-64 w-64 bottom-0 translate-y-10 blue-box rounded-full shadow-2xl"></div>
-          <div class="absolute h-40 w-40 bottom-0 right-0 red-box rounded-full opacity-50"></div>
-          <div class="absolute h-40 w-40 top-10 right-10 yellow-box rounded-full opacity-80"></div>
+          <div class="absolute h-72 w-72 bottom-0 right-20 translate-y-10 blue-box rounded-full shadow-2xl"></div>
+          <div class="absolute h-44 w-44 bottom-2 right-0 red-box rounded-full opacity-50"></div>
+          <div class="absolute h-40 w-40 top-6 right-10 yellow-box rounded-full opacity-80"></div>
         </div>
     </>
   )
